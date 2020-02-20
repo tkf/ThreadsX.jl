b15121ff4def71a5dde204709c65ba8486c94818
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-19 20:57:55 -0800

    Use Aqua.jl (#3)
    * Run Aqua.test_all(ThreadsX)
    
    * Make Aqua.test_all work
    
    * Revert "Make Aqua.test_all work"
    
    This reverts commit 30260b140a6e04df31e0e7153b34b05170843928.
    
    * Don't use Aqua.test_all to avoid upstream ambiguities

---
+++ test/test_aqua.jl
@@ -0,0 +1,22 @@
+module TestAqua
+
+import Aqua
+import ThreadsX
+using Test
+
+# Default `Aqua.test_all(ThreadsX)` does not work due to ambiguities
+# in upstream packages.
+
+@testset "Method ambiguity" begin
+    Aqua.test_ambiguities(ThreadsX)
+end
+
+@testset "Unbound type parameters" begin
+    Aqua.test_unbound_args(ThreadsX)
+end
+
+@testset "Undefined exports" begin
+    Aqua.test_undefined_exports(ThreadsX)
+end
+
+end  # module
