30260b140a6e04df31e0e7153b34b05170843928
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-19 20:48:35 -0800

    Make Aqua.test_all work

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,8 +1,13 @@
 module TestAqua
 
-using Aqua
-using ThreadsX
+import Aqua
+import Setfield
+import ThreadsX
 
-Aqua.test_all(ThreadsX)
+Aqua.test_all(
+    ThreadsX;
+    # https://github.com/JuliaCollections/DataStructures.jl/pull/511
+    ambiguities=(exclude=[Base.get, Setfield.set, Setfield.modify],),
+)
 
 end  # module
