0438e653f3529b3f4c359fd5d16ab750a5080c21
Takafumi Arakaki <aka.tkf@gmail.com>
2020-02-29 12:02:24 -0800

    Parallelize mergesorted!

--- test/test_sort.jl
+++ test/test_sort.jl
@@ -9,7 +9,27 @@ using ThreadsX
     @test ThreadsX.sort!(shuffle([1:1000;]), basesize = basesize, by = inv) == 1000:-1:1
     @test ThreadsX.sort!([1:1000;], basesize = basesize) == 1:1000
     # Sorting stability:
-    @test ThreadsX.sort!([1:1000;], basesize = basesize, by = _ -> 1) == 1:1000
+    @test ThreadsX.sort!(
+        [1:1000;],
+        basesize = basesize,
+        by = _ -> 1,
+        basesort! = (x; kw...) -> sort!(x; alg = MergeSort, kw...),
+    ) == 1:1000
+end
+
+@testset "stable sort" begin
+    @test ThreadsX.sort(
+        1:45;
+        basesize = 25,
+        by = _ -> 1,
+        basesort! = (x; kw...) -> sort!(x; alg = MergeSort, kw...),
+    ) == 1:45
+    @test ThreadsX.sort(
+        1:1000;
+        basesize = 200,
+        by = _ -> 1,
+        basesort! = (x; kw...) -> sort!(x; alg = MergeSort, kw...),
+    ) == 1:1000
 end
 
 end  # module
