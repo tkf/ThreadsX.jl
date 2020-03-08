d2fe6d8b00935f7f56799aa0e5f31468c2a1bf77
Takafumi Arakaki <aka.tkf@gmail.com>
2020-03-08 14:54:55 -0700

    Simplify benchmark/bench_sort.jl

--- benchmark/bench_sort.jl
+++ benchmark/bench_sort.jl
@@ -13,24 +13,13 @@ datasets = [
     ("sorted", [1:n;]),
 ]
 
-# modeling slow comparison:
-function slow_identity(a)
-    x = Float64(a)
-    for _ in 1:1
-        x = sin(x)
-    end
-    return a
-end
-
 suite = BenchmarkGroup()
 
 for (label, xs) in datasets
     suite[label] = BenchmarkGroup()
-    for by in [identity, slow_identity]
-        s = suite[label][string(by)] = BenchmarkGroup()
-        s["tx"] = @benchmarkable(ThreadsX.sort!(xs; by = $by), setup = (xs = copy($xs)))
-        s["base"] = @benchmarkable(sort!(xs; by = $by), setup = (xs = copy($xs)))
-    end
+    s = suite[label]
+    s["tx"] = @benchmarkable(ThreadsX.sort!(xs), setup = (xs = copy($xs)))
+    s["base"] = @benchmarkable(sort!(xs), setup = (xs = copy($xs)))
 end
 
 end  # module
