c3e7e0dc5aebf2089e28c4ef1369b209ee18fe93
Takafumi Arakaki <aka.tkf@gmail.com>
2020-03-09 01:39:25 -0700

    Add benchmark/bench_findfirst.jl (#46)

---
+++ benchmark/bench_findfirst.jl
@@ -0,0 +1,23 @@
+module BenchFindfirst
+
+import Random
+using BenchmarkTools
+using ThreadsX
+
+Random.seed!(1234)
+
+n = 2^18
+
+suite = BenchmarkGroup()
+
+xs0 = rand(n)
+for percent in [0, 10, 20, 30, 40, 50]
+    xs = copy(xs0)
+    xs[max(1, floor(Int, length(xs) * percent / 100))] = -1
+    s = suite["$(percent)%"] = BenchmarkGroup()
+    s["tx"] = @benchmarkable(ThreadsX.findfirst(==(-1), $xs))
+    s["base"] = @benchmarkable(findfirst(==(-1), $xs))
+end
+
+end  # module
+BenchFindfirst.suite
