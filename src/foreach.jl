67f126efc3dd52cc74b85db7ab6cce0b129d2db1
Takafumi Arakaki <aka.tkf@gmail.com>
2020-03-15 11:57:48 -0700

    Move map definitions to src/map.jl

--- src/foreach.jl
+++ src/foreach.jl
@@ -1,30 +1,3 @@
-__map(f, itr; kwargs...) =
-    tcollect(Map(f), itr; basesize = default_basesize(itr), kwargs...)
-__map(f, itrs...; kwargs...) =
-    tcollect(MapSplat(f), zip(itrs...); basesize = default_basesize(itrs[1]), kwargs...)
-
-function _map(f, itr, itrs...; kwargs...)
-    ys = __map(f, itr, itrs...; kwargs...)
-    isempty(ys) && return map(f, itr, itrs...)
-    return ys
-end
-
-ThreadsX.map(f, itr, itrs...; kwargs...) = _map(f, itr, itrs...; kwargs...)
-
-function ThreadsX.map(
-    f,
-    array::AbstractArray{<:Any,N},
-    arrays::AbstractArray{<:Any,N}...;
-    kwargs...,
-) where {N}
-    dims = size(array)
-    if !all(a -> size(a) == dims, arrays)
-        throw(ArgumentError("shape of arrays do not match"))
-    end
-    output = _map(f, array, arrays...; kwargs...)
-    return reshape(output, dims)
-end
-
 ThreadsX.foreach(f, xs::AbstractArray; basesize::Integer = default_basesize(xs)) =
     foreach_array(f, IndexStyle(xs), xs, basesize)
 
@@ -92,11 +65,3 @@ ThreadsX.foreach(
 ThreadsX.foreach(f, array::AbstractArray, arrays::AbstractArray; kw...) =
     ThreadsX.foreach(f, map(vec, tuple(array, arrays...))...; kw...)
 =#
-
-function ThreadsX.map!(f, dest, array, arrays...; kw...)
-    ThreadsX.foreach(referenceable(dest), array, arrays...; kw...) do y, xs...
-        Base.@_inline_meta
-        y[] = f(xs...)
-    end
-    return dest
-end
---
+++ src/map.jl
@@ -0,0 +1,34 @@
+__map(f, itr; kwargs...) =
+    tcollect(Map(f), itr; basesize = default_basesize(itr), kwargs...)
+__map(f, itrs...; kwargs...) =
+    tcollect(MapSplat(f), zip(itrs...); basesize = default_basesize(itrs[1]), kwargs...)
+
+function _map(f, itr, itrs...; kwargs...)
+    ys = __map(f, itr, itrs...; kwargs...)
+    isempty(ys) && return map(f, itr, itrs...)
+    return ys
+end
+
+ThreadsX.map(f, itr, itrs...; kwargs...) = _map(f, itr, itrs...; kwargs...)
+
+function ThreadsX.map(
+    f,
+    array::AbstractArray{<:Any,N},
+    arrays::AbstractArray{<:Any,N}...;
+    kwargs...,
+) where {N}
+    dims = size(array)
+    if !all(a -> size(a) == dims, arrays)
+        throw(ArgumentError("shape of arrays do not match"))
+    end
+    output = _map(f, array, arrays...; kwargs...)
+    return reshape(output, dims)
+end
+
+function ThreadsX.map!(f, dest, array, arrays...; kw...)
+    ThreadsX.foreach(referenceable(dest), array, arrays...; kw...) do y, xs...
+        Base.@_inline_meta
+        y[] = f(xs...)
+    end
+    return dest
+end
