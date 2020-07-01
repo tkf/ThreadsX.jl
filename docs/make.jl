f66210671dcc1917e3d8bd456678aa72ebb7a8b4
Takafumi Arakaki <aka.tkf@gmail.com>
2020-06-30 19:59:27 -0700

    Push preview only when `push_preview` tag is specified (#124)
    * Push preview only when `push_preview` tag is specified
    
    * Add JSON to docs/Project.toml

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,22 @@
-using Documenter, ThreadsX
+using Documenter
+using JSON
+using ThreadsX
+
+function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing))
+    event_path === nothing && return false
+    event = JSON.parsefile(event_path)
+    pull_request = get(event, "pull_request", nothing)
+    pull_request === nothing && return false
+    labels = [x["name"] for x in pull_request["labels"]]
+    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    yes = "push_preview" in labels
+    if yes
+        @info "Trying to push preview as label `push_preview` is specified." labels
+    else
+        @info "Not pushing preview as label `push_preview` is not specified." labels
+    end
+    return yes
+end
 
 makedocs(;
     modules=[ThreadsX],
@@ -13,5 +31,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/tkf/ThreadsX.jl",
-    push_preview=true,
+    push_preview = should_push_preview(),
 )
