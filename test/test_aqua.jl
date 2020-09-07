1cb471ff6257e9a7e44d399b6c63d8e35db69a24
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-07 16:55:12 -0700

    Use flags to control Aqua.test_all (#153)
    Use flag-based configuration added in Aqua 0.4.7.

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -6,25 +6,17 @@ using Test
 
 # Default `Aqua.test_all(ThreadsX)` does not work due to ambiguities
 # in upstream packages.
+Aqua.test_all(
+    ThreadsX;
+    ambiguities = false,
+    project_extras = true,
+    stale_deps = true,
+    deps_compat = true,
+    project_toml_formatting = true,
+)
 
 @testset "Method ambiguity" begin
     Aqua.test_ambiguities(ThreadsX)
-end
-
-@testset "Unbound type parameters" begin
-    Aqua.test_unbound_args(ThreadsX)
-end
-
-@testset "Undefined exports" begin
-    Aqua.test_undefined_exports(ThreadsX)
-end
-
-@testset "Compare Project.toml and test/Project.toml" begin
-    Aqua.test_project_extras(ThreadsX)
-end
-
-@testset "Stale dependencies" begin
-    Aqua.test_stale_deps(ThreadsX)
 end
 
 end  # module
